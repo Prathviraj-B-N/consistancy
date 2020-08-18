0fbf4171b70d61ed071250b2b9b364144b2d8529
prathviraj b <prathvirajbn.cs18@sahyadri.edu.in>
2020-08-18 11:38:34 +0530

    Create dynamicArray.cpp
    Dynamically creates array.

---
+++ dynamicArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+	int size;
+	int* array;
+	std::cout << "Enter size of array: ";
+	std::cin >> size;
+
+	array = new int(size);
+	std::cout << "Enter elements :\n";
+	for (int i = 0; i < size; i++) {
+		
+		std::cout << "[" << i + 1 << "/" << size << "] : ";
+		std::cin >> array[i];
+	}
+
+	std::cout << "Entered elements :\n";;
+
+	for (int i = 0; i < size; i++) {
+		std::cout << "[" << i + 1 << "/" << size << "] : ";
+		std::cout << array[i]<<"\n";
+	}
+	return *array;
+}
