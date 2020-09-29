507ceb8c372b65f5f103d3de150235791c40a092
prathviraj b <prathvirajbn.cs18@sahyadri.edu.in>
2020-09-29 11:05:37 +0530

    -_-_-_-_-_-

---
+++ 012.SelectionSort.cpp
@@ -0,0 +1,47 @@
+//time : O(n^2)
+//space: O(1) (internal sorting)
+//Stable sorting
+
+
+#include <iostream>
+
+void selection_sort(int* a, int n) {
+
+    for (int i = 0; i < n - 1; i++){
+        int min_index = i;
+        for (int j = i + 1; j < n; j++) {
+            if (a[min_index] > a[j]) {
+                min_index = j;
+            }
+        }
+        if (min_index != i) {
+            int temp = a[i];
+            a[i] = a[min_index];
+            a[min_index] = temp;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        std::cout << '[' << i + 1 << '/' << n << "]:";
+        std::cout << a[i]<<std::endl;
+    }
+}
+
+
+int main()
+{
+    short int SIZE;
+    std::cout << "Enter Size of the array:\n";
+    std::cin >> SIZE;
+    int* array = new int[SIZE];
+
+    std::cout << "Enter Elements:\n";
+    for (int i = 0; i < SIZE; i++) {
+        std::cout << '[' << i + 1 << '/' << SIZE << "]: ";
+        std::cin >> array[i];
+    }
+
+    selection_sort(array, SIZE);
+
+    return 0;
+}
