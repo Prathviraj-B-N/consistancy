42a0890d962ac07b980769beba879bafbb4268f3
prathviraj b <pishere.learn@gmail.com>
2021-04-17 18:54:15 +0530

    Create dreamCompany.java

---
+++ 031/dreamCompany.java
@@ -0,0 +1,22 @@
+package com.pishere;
+
+public class dreamCompany {
+    private String companyName;
+    int salary;
+
+    public String getCompanyName(){
+        return companyName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setCompanyName(String cn){
+        companyName = cn;
+    }
+}
