dde5f5270dad072b30fd7f9beddf8681572aefed
prathviraj b <pishere.learn@gmail.com>
2021-04-17 18:50:32 +0530

    Create dreamCompany.java

---
+++ 031/dreamCompany.java
@@ -0,0 +1,12 @@
+package com.pishere;
+
+public class dreamCompany {
+
+    public static void main(String[] args) {
+	// write your code here
+        dreamCompany microsoft = new dreamCompany();
+        microsoft.setCompanyName("MICROSOFT");
+        microsoft.salary = 240000;
+        System.out.println(microsoft.getCompanyName()+'\n'+microsoft.getSalary());
+    }
+}
